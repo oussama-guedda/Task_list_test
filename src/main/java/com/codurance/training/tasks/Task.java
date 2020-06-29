356c2df379ba5365866173a5a931fc6da59cd429
oussama-guedda <oussemagueddaa@gmail.com>
2020-06-29 10:39:33 +0200

    ajout attribut date sur la class task

--- src/main/java/com/codurance/training/tasks/Task.java
+++ src/main/java/com/codurance/training/tasks/Task.java
@@ -1,14 +1,27 @@
 package com.codurance.training.tasks;
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public final class Task {
     private final long id;
     private final String description;
     private boolean done;
+    private Date date = new Date();
 
-    public Task(long id, String description, boolean done) {
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Task(long id, String description, boolean done, Date date) {
         this.id = id;
         this.description = description;
         this.done = done;
+        this.date = date;
     }
 
     public long getId() {
